f5098e543f97ecc3f64c01ea1be4a6a4d07f02c3
Vincent Gourrierec <46958513+vincent-gou@users.noreply.github.com>
2020-07-17 12:28:37 +0200

    Update teaming info loop

--- net_info_extractor_linux.sh
+++ net_info_extractor_linux.sh
@@ -155,6 +155,8 @@ if [[ "$?" == "0" ]]
     if [[ "$?" == "0" ]]
       then
         TEAM_DEVICE=$(find /sys/class/net/* -lname "*team*" | sed -e "s/\// /g" | awk '{print $4}' )
+        echo -e "\t\t\t| Dev\t| Link\t| State\t| IP\t\t\t| Mask\t"
+        echo -e "\t\t\t ------- ------- ------- ----------------------- ------- \t"
         for TEAM_DEV in $TEAM_DEVICE
         do
           TEAM_DEVICE_LINK_STATE=$(cat /sys/class/net/$TEAM_DEV/carrier 2>/dev/null >/dev/null && printf "ok" || printf "ko")
@@ -162,8 +164,6 @@ if [[ "$?" == "0" ]]
           #TEAM_DEVICE_SPEED=$(cat /sys/class/net/$DEV/speed 2>/dev/null  || printf "down")
           TEAM_DEVICE_IP=$(ip -o -4 addr show dev $TEAM_DEV | cut -d ' ' -f 7  | cut -f 1 -d '/' 2>/dev/null || printf "down")
           TEAM_DEVICE_NETMASK=$(ip -o -4 addr show dev $TEAM_DEV | cut -d ' ' -f 7  | cut -f 2 -d '/' 2>/dev/null || print "down")
-          echo -e "\t\t\t| Dev\t| Link\t| State\t| IP\t\t\t| Mask\t"
-          echo -e "\t\t\t ------- ------- ------- ----------------------- ------- \t"
           printf "Teaming_Net_Device:\t" | tee -a $CONF_OUTPUT
           printf "| $TEAM_DEV\t" | tee -a $CONF_OUTPUT
           printf "| $TEAM_DEVICE_LINK_STATE \t" | tee -a $CONF_OUTPUT
